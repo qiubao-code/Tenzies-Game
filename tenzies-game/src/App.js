953bc03a793c882f609f17f406d75ef32dbb1f12
qiubao-code <1853413171@qq.com>
2022-08-04 11:17:18 +0800

    roll dice function

--- tenzies-game/src/App.js
+++ tenzies-game/src/App.js
@@ -1,42 +1,29 @@
 import React from "react";
 import Die from "./Die"
 
-export default function App(){
-    const value = 1
+export default function App() {
+    const [dice, setDice] = React.useState(allNewDice());
+    function allNewDice() {
+        const newValues = [];
+        for(let i = 0;i<10;i++){
+            newValues.push(Math.floor(Math.random()*6)+1)
+        }
+        return newValues;
+    }
+    function rollDice(){
+        setDice(allNewDice());
+    }
+    console.log(dice);
+    const diceElements=dice.map(item=> <Die value={item}/>)
+
     return (
         <main>
             <div className="dice-container">
-                <Die 
-                    value={value}
-                />
-                <Die 
-                    value={value}
-                />
-                <Die 
-                    value={value}
-                />
-                <Die 
-                    value={value}
-                />
-                <Die 
-                    value={value}
-                />
-                <Die 
-                    value={value}
-                />
-                <Die 
-                    value={value}
-                />
-                <Die 
-                    value={value}
-                />
-                <Die 
-                    value={value}
-                />
-                <Die 
-                    value={value}
-                />
+                {diceElements}
             </div>
+            <button
+                onClick={rollDice}
+            >Roll</button>
         </main>
     )
 }
