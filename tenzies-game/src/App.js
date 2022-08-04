8a7297eddd34fe688115de4e4443631a7b070966
qiubao-code <1853413171@qq.com>
2022-08-04 15:45:42 +0800

    holdDice upddate

--- tenzies-game/src/App.js
+++ tenzies-game/src/App.js
@@ -1,28 +1,48 @@
 import React from "react";
 import Die from "./Die"
+import { nanoid } from "nanoid"
 
 export default function App() {
     const [dice, setDice] = React.useState(allNewDice());
     function allNewDice() {
         const newValues = [];
-        for(let i = 0;i<10;i++){
-            newValues.push(Math.floor(Math.random()*6)+1)
+        for (let i = 0; i < 10; i++) {
+            const randomnum = Math.floor(Math.random() * 6) + 1
+            newValues.push({
+                value: randomnum,
+                isheld: true,
+                id: nanoid()
+            })
         }
         return newValues;
     }
-    function rollDice(){
+
+    function rollDice() {
         setDice(allNewDice());
     }
-    console.log(dice);
-    const diceElements=dice.map(item=> <Die value={item}/>)
+    
+    function holdDice(id){
+        console.log(id);
+    }
+
+    const diceElements = dice.map(item => (
+        <Die 
+            key={item.id} 
+            id={item.id}
+            value={item.value} 
+            isheld={item.isheld}
+            holdDice={()=>holdDice(item.id)}
+        />
+    ))
 
     return (
         <main>
-            <div className="dice-container">
+            <div className={"dice-container"}>
                 {diceElements}
             </div>
             <button
                 onClick={rollDice}
+                className="roll-dice"
             >Roll</button>
         </main>
     )
